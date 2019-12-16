332077c241270d3009a8dabfbc9b805ef36721bd
Alexandr <48840017+Zhukovdpua@users.noreply.github.com>
2019-12-16 12:26:33 +0300

    Update main.cpp

--- Поиск компонент связности в графе/main.cpp
+++ Поиск компонент связности в графе/main.cpp
@@ -4,15 +4,14 @@
 
 
 int main() {
-	string name = "graph.txt";
 	ifstream in;
-	in.open(name);
+	in.open("graph.txt");
 	string s;
 	in >> s;
 	int tof = stoi(s);
 	in.close();
 
-	Multy_connected_component g(tof, name);
+	Multy_connected_component g(tof, "graph.txt");
 	auto future = g.compute(thread::hardware_concurrency() - 1);
 	cout << endl << "Connected component count: " << future.get() << endl;
 	return 0;
